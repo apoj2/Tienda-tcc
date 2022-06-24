dd2fa999486ad7fd279f0c62c58b416807102291
Your Name <you@example.com>
2022-06-24 12:37:12 -0500

    Version Principal

---
+++ src/controllers/controladorindex.js
@@ -0,0 +1,12 @@
+let nombre="Juan"
+
+
+//controlador de etiquetas
+//1.Necesito almacenar una etiqueta en una variable
+let titulo=document.getElementById("titulo1")
+let imagen=document.getElementById('foto')
+//2.Identifico que quiero controlar
+//2.1 cambiando el texto
+titulo.textContent="chao "
+//2.2 cambiando la fuente
+imagen.src='https://placeimg.com/640/480/any'
