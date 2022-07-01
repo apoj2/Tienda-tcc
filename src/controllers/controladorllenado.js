8696f274dfe4cf43a192a81b376d582de8342081
saramontoya24@gmail.com <you@example.com>
2022-07-01 12:12:05 -0500

    Se crearon los archivos para el controlador

---
+++ src/controllers/controladorllenado.js
@@ -0,0 +1,12 @@
+// datos de la bd
+
+let productosBD=[
+{
+    nombre:"Brazo",
+    precio:55000,
+    cantidad:0,
+    descripcion:"Objeto que va en la base",
+    fotos:['https://firebasestorage.googleapis.com/v0/b/apojtiendatcc.appspot.com/o/BrazoLogo.png?alt=media&token=9ed8bd23-939b-4498-9972-b3f528032722','../../assets/img/Imagenes/LogosServicios/SistemaAutomaticoVolqueta/BaseLogo.png']
+},
+{},
+]
