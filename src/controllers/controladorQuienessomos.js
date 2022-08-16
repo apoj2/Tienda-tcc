e3b6242c9861ebadb4240614a3f78248fa9702a6
201 <201@academicos.cesde.edu.co>
2022-08-16 09:44:45 -0500

    organizamos el input

--- src/controllers/controladorQuienessomos.js
+++ src/controllers/controladorQuienessomos.js
@@ -1,7 +1,18 @@
 import {botonescategoriasproductos} from '../helpers/botonescategorias.js'
 import {llenarnavcategorias} from './controladorllenadonavcategorias.js'
 import{navegacionproductos}from './controladorClicAmpliarNavegacion.js'
+import {llenarProductos} from './controladorllenado.js'
 
+
+import {sistemaautomaticovolqueta,sistemaautomaticovolquetacarrusel1,sistemaautomaticovolquetacarrusel2,sistemaautomaticovolquetacarrusel3,sistemaautomaticovolquetaproductos} from '../helpers/databasesistemaautomaticovolqueta.js'
+import {sistemamanualvolqueta} from '../helpers/databasesistemamanualvolqueta.js'
+import {sistemaautomaticommula} from '../helpers/databasesistemaautomaticomula.js'
+import {sistemamanualmula} from '../helpers/databasesistemamanualmula.js'
+import {protectoreslaterales,protectoreslateralesproductos} from '../helpers/databaseprotectoreslaterales.js'
+import {cintareflectiva,cintareflectivaproductos} from '../helpers/databasecintareflectiva.js'
+import {discosabrasivos,discosabrasivosproductos} from '../helpers/databasediscosabrasivos.js'
+import {malacates} from '../helpers/databasemalacates.js'
+import {repuestos} from '../helpers/databaserepuestos.js'
 
 
 llenarnavcategorias('navcategorias',botonescategoriasproductos)
@@ -12,14 +23,14 @@ let cantidadcarrito=document.getElementById("cantidadcarrito")
 cantidadcarrito.textContent=cantidad
 
 
-let navcategoriastorage=localStorage.getItem('navcategorias')
-console.log()
+
 let navcategorias=document.getElementById("navcategorias")
 
  navcategorias.addEventListener("click",function(evento){
     
         let botones = navegacionproductos(evento)
         console.log(botones)
+        
 
         if(botones.categoria=='1'){
 			window.location.href='./tienda.html#tienda'
